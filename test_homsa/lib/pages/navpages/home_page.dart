8cb5762ae775e204edf2ce689c974cbe2021410b
bishoyy <mkk4434@gmail.com>
2023-12-27 14:37:54 +0200

    all added again

---
+++ test_homsa/lib/pages/navpages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:test_homsa/widgets/app_large_text.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //menu text
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                Icon(Icons.menu, size: 30, color: Colors.black54),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          //discover text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          //tabbar
+          Container(
+            child: TabBar(tabs: [
+              Tab(text: "Places"),
+              Tab(text: "Inspiration"),
+              Tab(text: "Emotions")
+            ]),
+          ),
+          Container()
+        ],
+      ),
+    );
+  }
+}
